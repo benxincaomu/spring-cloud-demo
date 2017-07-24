79b00f883291fbc642a5cd284da274d9626fdfd0
benxincaomu <benxincaomu@foxmail.com>
2017-07-24 09:54:39 +0800

    被调用服务输出日志

--- base-service/src/main/java/org/base/service/main/controller/TestServiceController.java
+++ base-service/src/main/java/org/base/service/main/controller/TestServiceController.java
@@ -1,13 +1,17 @@
 package org.base.service.main.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestServiceController {
 
+    private final Logger log=LogManager.getLogger(this.getClass()); 
     @GetMapping("service")
     public String service() {
+        log.info("我被调用了");
         return "This is a service";
     }
 
