629f4ce1b2bc51c39cd3ccba3ed1a954557bda2d
benxincaomu <benxincaomu@foxmail.com>
2017-08-02 17:12:19 +0800

    可刷新配置（手动）

--- config-client/src/main/java/org/web/App.java
+++ config-client/src/main/java/org/web/App.java
@@ -2,13 +2,14 @@ package org.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-
+@EnableEurekaClient
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
--- config-server/src/main/java/org/config/App.java
+++ config-server/src/main/java/org/config/App.java
@@ -3,13 +3,13 @@ package org.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-public class App 
-{
-    public static void main( String[] args )
-    {
+@EnableEurekaClient
+public class App {
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 }
