20029aa7f1028c6b391aa357291d91231ad33fa7
benxincaomu <benxincaomu@foxmail.com>
2017-08-16 23:46:03 +0800

    统一jar包版本

--- eureka-service/src/main/java/org/base/service/main/controller/TestServiceController.java
+++ eureka-service/src/main/java/org/base/service/main/controller/TestServiceController.java
@@ -5,7 +5,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class TestServiceController {
